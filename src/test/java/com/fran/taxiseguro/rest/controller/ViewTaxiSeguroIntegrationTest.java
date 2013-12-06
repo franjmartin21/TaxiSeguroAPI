8eae8ac0f26bf59bc280f2eb5d8d4725b9f9ba14
Fran <franjmartin21@gmail.com>
2013-12-06 07:05:18 -0500

    Test of Rest Controller

---
+++ src/test/java/com/fran/taxiseguro/rest/controller/ViewTaxiSeguroIntegrationTest.java
@@ -0,0 +1,53 @@
+package com.fran.taxiseguro.rest.controller;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fran.taxiseguro.domain.TaxiStart;
+import com.fran.taxiseguro.services.TaxiSeguroService;
+
+
+public class ViewTaxiSeguroIntegrationTest {
+	MockMvc mockMvc;
+	
+	@InjectMocks
+	private TaxiSeguroQueriesController controller = new TaxiSeguroQueriesController();
+	
+	@Mock
+	private TaxiSeguroService taxiSeguroService;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+
+		this.mockMvc = standaloneSetup(controller).setMessageConverters(new MappingJackson2HttpMessageConverter()).build();
+	}
+	
+	@Test
+	public void thatTaxiStartHttpOk() throws Exception {
+ 
+	    when(taxiSeguroService.listTaxiStart()).thenReturn(new ArrayList<TaxiStart>());
+
+	    this.mockMvc.perform(
+	            get("/taxistart")
+	                    .accept(MediaType.APPLICATION_JSON))
+	            .andDo(print())
+	            .andExpect(status().isOk());
+	  }
+
+}
